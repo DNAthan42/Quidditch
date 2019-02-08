a7fcec68bdd21412e3018b88b34bef5bc34b9ada
DNAthan42 <douglasnathan42@gmail.com>
2019-02-07 22:41:29 -0700

    Brooms now chase the snitch.

---
+++ Assets/Scripts/Broom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Broom : MonoBehaviour
+{
+    Rigidbody rb;
+    public Transform snitch;
+
+    float maxVelocity = 20;
+    float maxAccel = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(snitch);
+        rb.AddRelativeForce(Vector3.forward * maxAccel);
+        //Capping velocity
+        //https://answers.unity.com/questions/683158/how-to-limit-speed-of-a-rigidbody.html
+        if (rb.velocity.magnitude > maxVelocity)
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+    }
+}

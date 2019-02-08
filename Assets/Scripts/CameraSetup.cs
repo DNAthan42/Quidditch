89923a55781be59e0519e10185735c094b6a266b
DNAthan42 <douglasnathan42@gmail.com>
2019-02-08 16:55:10 -0700

    Can now toggle between the default camera and an action camera which follows snitch and looks behind it. Press space to toggle

--- Assets/Scripts/CameraSetup.cs
+++ Assets/Scripts/CameraSetup.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class CameraSetup : MonoBehaviour
 {
     public Transform snitch;
+    private bool action;
+    private Vector3 InitialPosition;
+    private Vector3 InitialAngle;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        action = false;
+        InitialAngle = transform.eulerAngles;
+        InitialPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.LookAt(snitch);
+        if (Input.GetKeyDown(KeyCode.Space)) SwapCam();
+    }
+
+    private void SwapCam()
+    {
+        if (action)
+        {
+            transform.parent = null;
+            transform.eulerAngles = InitialAngle;
+            transform.position = InitialPosition;
+            action = false;
+        }
+        else
+        {
+            transform.parent = snitch;
+            transform.localEulerAngles = new Vector3(0, 180, 0);
+            transform.localPosition = Vector3.zero;
+            action = true;
+        }
     }
 }
